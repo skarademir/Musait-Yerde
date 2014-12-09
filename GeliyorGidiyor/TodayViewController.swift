db50c0d1b0a3c55817810a018f4925c3dcbeec4e
skarademir <saruhan@MacBook-Pro.local>
2014-12-08 20:01:03 -0800

    Initial switch to UITableView

--- GeliyorGidiyor/TodayViewController.swift
+++ GeliyorGidiyor/TodayViewController.swift
@@ -12,22 +12,27 @@ import NotificationCenter
 import CoreLocation
 import SwiftyJSON
 
+class TodayViewController: UITableViewController, CLLocationManagerDelegate, NCWidgetProviding {
 
-class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidgetProviding {
-    @IBOutlet var NearbyOne_RouteNum: UILabel!
-    @IBOutlet var NearbyOne_Minutes: UILabel!
-    @IBOutlet var NearbyOne_ArrivalTime: UILabel!
     
-    @IBOutlet var NearbyTwo_RouteNum: UILabel!
-    @IBOutlet var NearbyTwo_Minutes: UILabel!
-    @IBOutlet var NearbyTwo_ArrivalTime: UILabel!
-    
-    @IBOutlet var Fav_RouteNum: UILabel!
-    @IBOutlet var Fav_Minutes: UILabel!
-    @IBOutlet var Fav_ArrivalTime: UILabel!
+    struct TableViewConstants {
+        static let baseRowCount = 3
+        static let todayRowHeight = 75
+        
+        struct CellIdentifiers {
+            static let content = "busViewCell"
+            static let message = "messageCell"
+        }
+    }
     
     
     let locationManager = CLLocationManager()
+    var favoriteList = Array<SwiftyJSON.JSON>()
+    var favoriteDirectionList = Array<SwiftyJSON.JSON>()
+    var routeList = Array<SwiftyJSON.JSON>()
+    var routeDirectionList = Array<SwiftyJSON.JSON>()
+    
+    // MARK: View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +45,7 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
         var favStopId: String = "1_13460"
         //favStopId = sharedDefaults!.objectForKey("numberPass") as String
         //sharedDefaults.
+        
         println(favStopId)
         
         updateFavStop(favStopId,favRouteId: "40_100236")
@@ -48,6 +54,7 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
         locationManager.startUpdatingLocation()
         
     }
+    
     func updateFavStop(favStopId: String , favRouteId: String) {
         Alamofire.request(.GET, "http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/\(favStopId).json?key=org.onebusaway.iphone&version=2")
             .responseJSON { (_, _, data, error) in
@@ -71,33 +78,24 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
                     println ("\(didntArriveYet && onFavRoute)");
                     return didntArriveYet && onFavRoute })
                 favstop_buses.sort({$0["predictedArrivalTime"].intValue < $1["predictedArrivalTime"].intValue})
-                
                 if favstop_buses.count > 0 {
                     //Build the RouteNum by combining Bus Number and Bus Compass direction)
+                    self.favoriteList.append(favstop_buses[0])
                     let busNum = favstop_buses[0]["routeShortName"]
                     let busDirection = responseJSON["data"]["references"]["stops"][0]["direction"]
-                    self.Fav_RouteNum.text = "\(busNum)\(busDirection)"
-                    
-                    //Json provides epoch time in milliseconds were converting to seconds.
-                    let busPredictedArrivalEpochTime = favstop_buses[0]["predictedArrivalTime"].doubleValue/1000
-                    let now = NSDate()
-                    
-                    //Display Minutes to bus arrival
-                    //I could use NSDate to compare now and arrival, i shoudldo that maybe?
-                    //this works for now TODO
-                    self.Fav_Minutes.text = String(Int((busPredictedArrivalEpochTime - now.timeIntervalSince1970)/60))
-                    
-                    //Display arrival time in 12:00 AM format
-                    let arrival = NSDate(timeIntervalSince1970: busPredictedArrivalEpochTime)
-                    let hhmmFormatter = NSDateFormatter()
-                    hhmmFormatter.dateFormat = "h:mm a"
-                    self.Fav_ArrivalTime.text = hhmmFormatter.stringFromDate(arrival)
+                    self.favoriteDirectionList.append(busDirection)
                 }
         }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: NCWidgetProviding
+    
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsets(top: defaultMarginInsets.top, left: 27.0, bottom: defaultMarginInsets.bottom, right: defaultMarginInsets.right)
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
@@ -115,10 +113,12 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
         
         completionHandler(NCUpdateResult.NewData)
     }
-    
+    // MARK: CLLocationManagerDelegate
     func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
-        //test if authorization was given, if so try to determine Nearby Routes
         
+        //stop polling for locations after first time (don't expect hte user to move particularly far before closing the today widget) TODO
+        locationManager.stopUpdatingLocation()
+
         
         //println("locations = \(locations)")
         var latValue = locationManager.location.coordinate.latitude
@@ -176,22 +176,10 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
                                 let busNum = stop0_buses[0]["routeShortName"]
 
                                 let busDirection = stops[0]["direction"]
-                                self.NearbyOne_RouteNum.text = "\(busNum)\(busDirection)"
-                                
-                                //Json provides epoch time in milliseconds were converting to seconds.
-                                let busPredictedArrivalEpochTime = stop0_buses[0]["predictedArrivalTime"].doubleValue/1000
-                                let now = NSDate()
-                                
-                                //Display Minutes to bus arrival
-                                //I could use NSDate to compare now and arrival, i shoudldo that maybe?
-                                //this works for now TODO
-                                self.NearbyOne_Minutes.text = String(Int((busPredictedArrivalEpochTime - now.timeIntervalSince1970)/60))
-                                
-                                //Display arrival time in 12:00 AM format
-                                let arrival = NSDate(timeIntervalSince1970: busPredictedArrivalEpochTime)
-                                let hhmmFormatter = NSDateFormatter()
-                                hhmmFormatter.dateFormat = "h:mm a"
-                                self.NearbyOne_ArrivalTime.text = hhmmFormatter.stringFromDate(arrival)
+                                self.routeList.append(stop0_buses[0])
+                                self.routeDirectionList.append(busDirection)
+                                println("C2S0 RouteList count: \(self.routeList.count)")
+                                self.tableView.reloadData()
                             }
                     }
                             //println("\(stop1_id)")
@@ -217,23 +205,15 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
                                         //Build the RouteNum by combining Bus Number and Bus Compass direction)
                                         let busNum = stop1_buses[0]["routeShortName"]
                                         let busDirection = stops[1]["direction"]
-                                        self.NearbyTwo_RouteNum.text = "\(busNum)\(busDirection)"
                                         
-                                        //Json provides epoch time in milliseconds were converting to seconds.
-                                        let busPredictedArrivalEpochTime = stop1_buses[0]["predictedArrivalTime"].doubleValue/1000
-                                        let now = NSDate()
                                         
-                                        //Display Minutes to bus arrival
-                                        //I could use NSDate to compare now and arrival, i shoudldo that maybe?
-                                        //this works for now TODO
-                                        self.NearbyTwo_Minutes.text = String(Int((busPredictedArrivalEpochTime - now.timeIntervalSince1970)/60))
+                                        self.routeList.append(stop1_buses[0])
+                                        self.routeDirectionList.append(busDirection)
                                         
-                                        //Display arrival time in 12:00 AM format
-                                        let arrival = NSDate(timeIntervalSince1970: busPredictedArrivalEpochTime)
-                                        let hhmmFormatter = NSDateFormatter()
-                                        hhmmFormatter.dateFormat = "h:mm a"
-                                        self.NearbyTwo_ArrivalTime.text = hhmmFormatter.stringFromDate(arrival)
+                                        println("C2S1 RouteList count: \(self.routeList.count)")
                                     }
+                                    
+                                    self.tableView.reloadData()
 
                     
                     }
@@ -263,7 +243,6 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
                             
                             if stop0_buses.count > 0 {
                                 //we pick the first bus that is predicted to arrive AFTER current time
-                                //yes this is ghetto TODO
                                 var i: Int = 0
                                 var i_arrival: Int = stop0_buses[i]["predictedArrivalTime"].intValue
                                 while  i_arrival < currentTime {
@@ -277,55 +256,125 @@ class TodayViewController: UIViewController, CLLocationManagerDelegate, NCWidget
                                 //Build the RouteNum by combining Bus Number and Bus Compass direction)
                                 let busNum = stop0_buses[0]["routeShortName"]
                                 let busDirection = stops[0]["direction"]
-                                self.NearbyOne_RouteNum.text = "\(busNum)\(busDirection)"
-                                
-                                //Json provides epoch time in milliseconds were converting to seconds.
-                                let busPredictedArrivalEpochTime = stop0_buses[0]["predictedArrivalTime"].doubleValue/1000
-                                let now = NSDate()
-                                
-                                //Display Minutes to bus arrival
-                                //I could use NSDate to compare now and arrival, i shoudldo that maybe?
-                                //this works for now TODO
-                                self.NearbyOne_Minutes.text = String(Int((busPredictedArrivalEpochTime - now.timeIntervalSince1970)/60))
-                                
-                                //Display arrival time in 12:00 AM format
-                                let arrival = NSDate(timeIntervalSince1970: busPredictedArrivalEpochTime)
-                                let hhmmFormatter = NSDateFormatter()
-                                hhmmFormatter.dateFormat = "h:mm a"
-                                self.NearbyOne_ArrivalTime.text = hhmmFormatter.stringFromDate(arrival)
-                                                                if stop0_buses.count > 1 {
+                                self.routeList.append(stop0_buses[0])
+                                self.routeDirectionList.append(busDirection)
+                                println("C1B0 RouteList count: \(self.routeList.count)")
+                                if stop0_buses.count > 1
+                                {
                                 //Build the RouteNum by combining Bus Number and Bus Compass direction)
                                 let bus2Num = stop0_buses[1]["routeShortName"]
-                                let bus2Direction = stops[1]["direction"]
-                                self.NearbyTwo_RouteNum.text = "\(busNum)\(busDirection)"
+                                self.routeList.append(stop0_buses[1])
+                                self.routeDirectionList.append(busDirection)
+                                println("C1B1 RouteList count: \(self.routeList.count)")
+                                }
                                 
-                                //Json provides epoch time in milliseconds were converting to seconds.
-                                let bus2PredictedArrivalEpochTime = stop0_buses[1]["predictedArrivalTime"].doubleValue/1000
-                                
-                                
-                                //Display Minutes to bus arrival
-                                //I could use NSDate to compare now and arrival, i shoudldo that maybe?
-                                //this works for now TODO
-                                self.NearbyTwo_Minutes.text = String(Int((busPredictedArrivalEpochTime - now.timeIntervalSince1970)/60))
-                                
-                                //Display arrival time in 12:00 AM format
-                                let arrival2 = NSDate(timeIntervalSince1970: busPredictedArrivalEpochTime)
-                                self.NearbyTwo_ArrivalTime.text = hhmmFormatter.stringFromDate(arrival)}
+                                //self.tableView.reloadData()
                             }
                     }
                 case 0:
                     println("Case 0")
                 default:
+                    
                     println("Case Default")
                 }
                 
                 
                 
         }
-        //stop polling for locations after first time (don't expect hte user to move particularly far before closing the today widget) TODO
-        locationManager.stopUpdatingLocation()
-        
-        
     }
+    
+    // MARK: UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(_ tableView: UITableView) -> Int {
+        return 2
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 1:
+            if routeList.count < 1 {
+                return 1
+            }
+            return min(routeList.count, TableViewConstants.baseRowCount + 1)
+        case 0:
+            return favoriteList.count
+        default:
+            return 0
+        }
+    }
+
+    override func tableView(_: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.layer.backgroundColor = UIColor.clearColor().CGColor
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if routeList.count > 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier(TableViewConstants.CellIdentifiers.content, forIndexPath: indexPath) as busViewCell
+            if indexPath.section == 1 {
+                configureBusItemCell(cell, busJSON: routeList[indexPath.row], busDirection: routeDirectionList[indexPath.row])
+                tableView.headerViewForSection(indexPath.section)?.textLabel.text = "Nearby Routes"
+            }
+            else {
+                configureBusItemCell(cell, busJSON: favoriteList[indexPath.row], busDirection: favoriteDirectionList[indexPath.row])
+                tableView.headerViewForSection(indexPath.section)?.textLabel.text = "Favorite Route"
+            }
+
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCellWithIdentifier(TableViewConstants.CellIdentifiers.content, forIndexPath: indexPath) as UITableViewCell
+                cell.textLabel.text = NSLocalizedString("No items in today's list", comment: "")
+
+            return cell
+        }
+    }
+    
+    func configureBusItemCell(itemCell: busViewCell, busJSON: SwiftyJSON.JSON, busDirection: SwiftyJSON.JSON) {
+        let busNum = busJSON["routeShortName"]
+        
+        itemCell.RouteNum.text = "\(busNum)\(busDirection)"
+        
+        //Json provides epoch time in milliseconds were converting to seconds.
+        let busPredictedArrivalEpochTime = busJSON["predictedArrivalTime"].doubleValue/1000
+        let now = NSDate()
+        
+        //Display Minutes to bus arrival
+        //I could use NSDate to compare now and arrival, i shoudldo that maybe?
+        //this works for now TODO
+        itemCell.Minutes.text = String(Int((busPredictedArrivalEpochTime - now.timeIntervalSince1970)/60))
+        
+        //Display arrival time in 12:00 AM format
+        let arrival = NSDate(timeIntervalSince1970: busPredictedArrivalEpochTime)
+        let hhmmFormatter = NSDateFormatter()
+        hhmmFormatter.dateFormat = "h:mm a"
+        itemCell.ArrivalTime.text = hhmmFormatter.stringFromDate(arrival)
+    }
+    
+    
+    
+    // MARK: UITableViewDelegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.reloadData()
+        /*
+            tableView.beginUpdates()
+            let indexPathForRemoval = NSIndexPath(forRow: 0, inSection: 0)
+            tableView.deleteRowsAtIndexPaths([indexPathForRemoval], withRowAnimation: .Fade)
+        
+        
+        
+            let insertedIndexPathRange = 0..<routeList.count
+            var insertedIndexPaths = insertedIndexPathRange.map { NSIndexPath(forRow: $0, inSection: 0) }
+        
+        
+        
+        tableView.insertRowsAtIndexPaths(insertedIndexPaths, withRowAnimation: .Fade)
+            tableView.endUpdates()
+            */
+        
+    
+    }
+    
+    
+
     
 }
---
+++ GeliyorGidiyor/busViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  busViewCell.swift
+//  Musait Yerde
+//
+//  Created by Saruhan on 23-11-14.
+//  Copyright (c) 2014 KaraBal. All rights reserved.
+//
+
+import UIKit
+
+class busViewCell: UITableViewCell {
+    
+    @IBOutlet var RouteNum: UILabel!
+    @IBOutlet var Minutes: UILabel!
+    @IBOutlet var ArrivalTime: UILabel!
+}
